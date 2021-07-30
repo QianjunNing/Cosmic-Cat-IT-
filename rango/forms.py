2435ee8b046105d79e57a93f26654ca85398f080
Qianjun Ning <2584096N@student.gla.ac.uk>
2021-07-30 01:48:22 +0100

    chapter7 clean

--- rango/forms.py
+++ rango/forms.py
@@ -22,5 +22,15 @@ class PageForm(forms.ModelForm):
         model = Page
         exclude = ('category',)
 
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+
+        if url and not url.startswith('http://'):
+            url = f'http://{url}'
+            cleaned_data['url'] = url
+        
+        return cleaned_data
+
     
     
